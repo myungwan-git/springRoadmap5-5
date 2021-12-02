7e78e9a4a7c5d38f00508c3f2f37227eff9c914a
myungwan-git <nsubway1070635@gmail.com>
2021-12-02 09:06:49 +0900

    20211201 spring roadmap5-5 login START

---
+++ src/main/java/hello/login/TestDataInit.java
@@ -0,0 +1,38 @@
+package hello.login;
+
+import hello.login.domain.item.Item;
+import hello.login.domain.item.ItemRepository;
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class TestDataInit {
+
+    private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
+
+    /**
+     * 테스트용 데이터 추가
+     */
+    @PostConstruct
+    public void init() {
+        log.info(" >>> PostConstruct Excute Save Member @@@@@@");
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+
+        Member member = new Member();
+        member.setLoginId("test");
+        member.setPassword("test!");
+        member.setName("테스터");
+
+        memberRepository.save(member);
+
+    }
+
+}
